c78b2a4edf3bcf39b31ca2280c6a5534f9017a9f
BELLOUT Abdelkrim <krimou9718@gmail.com>
2022-03-02 15:16:52 +0100

    first commit

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function ImgSlider() {
+  return (
+    <Rig>
+        ImgSlider
+    </Rig>
+  )
+}
+
+export default ImgSlider
+
+const Rig = styled.div`
+    background: orange;
+    display: flex;
+    justify-content: center;
+    align-item: center;
+    width: 100%;
+    height: 400px;
+`
